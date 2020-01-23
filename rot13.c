6e795d7e4e7c9fe003864add1e89db6e6741a609
Amon Munyai <amunyai@c4r5s7.wethinkcode.co.za>
2020-01-23 11:59:16 +0200

    Dont quit

--- rot13.c
+++ rot13.c
@@ -1,42 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rev_print.c                                        :+:      :+:    :+:   */
+/*   rot13.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amunyai <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/23 09:35:20 by amunyai           #+#    #+#             */
-/*   Updated: 2020/01/23 11:43:59 by amunyai          ###   ########.fr       */
+/*   Created: 2020/01/23 11:46:48 by amunyai           #+#    #+#             */
+/*   Updated: 2020/01/23 11:58:03 by amunyai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-//int			ft_strlen(char *str)
-//{
-//	int i;
-//
-//	i = 0;
-//	while (str[i] != '\0')
-//	{
-//		i++;
-//	}
-//	return (i);
-//}
-
 int			main(int argc, char *argv[])
 {
 	int i;
-//	int len;
 
 	i = 0;
 	if (argc > 1)
 	{
-//		len = ft_strlen(argv[1] - 1);
-		while (argv[1][i - 1] != '\0')
+		while (argv[1][i] != '\0')
 		{
-			write(1, &argv[1][i - 1], 1);
-			i--;
+			write(1, &argv[1][i], 1);
+			i++;
 		}
 	}
 	write(1, "\n", 1);
